aaeb68837357db2f4bd8bbb87ec988d3715f6d9c
doctormartin67 <celticfans@hotmail.com>
2020-08-16 14:11:13 +0200

    nEx implemented + updated test program (npx and nEx)

--- actuarial/actuarialfunctions.c
+++ actuarial/actuarialfunctions.c
@@ -1,7 +1,7 @@
+#include <math.h>
 #include "lifetables.h"
 #include "actuarialfunctions.h"
 
-// npx is the chance for some of age ageX to live until the age of ageXn
 double npx(char *lt, double ageX, double ageXn, int corr){ //lt = life table
   /* generally the rule is npx = lx(ageXn)/lx(ageX) but because lx has
      an integer value as its input we need to interpolate both these
@@ -17,4 +17,12 @@ double npx(char *lt, double ageX, double ageXn, int corr){ //lt = life table
   ip2 = lx(lt, ageXn) - (ageXn - (int)ageXn) * (lx(lt, ageXn) - lx(lt, ageXn+1));
   
   return ip2/ip1;
+}
+
+double nEx(char *lt, double i, double charge, double ageX, double ageXn, int corr){
+  double im = (1+i)/(1+charge) - 1;
+  double n = ageXn - ageX;
+  double vn = 1/pow(1 + im, n);
+  double nPx = npx(lt, ageX, ageXn, corr);
+  return vn * nPx;
 }
--- actuarial/actuarialfunctions.h
+++ actuarial/actuarialfunctions.h
@@ -1,1 +1,19 @@
-double npx(char *lifetable, double ageX, double ageXn, int corr);
+/*
+Definitions:
+- lt is the name of a life table that gets put in a hashtable to be called.
+  within the hash table element there is a int array with lx values. These lx 
+  values define the amount of people alive at a certain age (the array index).
+- ageX is the age to start.
+- ageXn is the age to finish at.
+- corr is a correction that can be made with age. For example corr = -5 would mean ageX-=5
+  and ageXn-=5
+- i is the discount rate.
+- charge is a possible charge that an insurance company or another entity might charge on
+  the discount rate.
+ */
+
+// npx is the chance for some of age ageX to live until the age of ageXn
+double npx(char *lt, double ageX, double ageXn, int corr);
+// nEx is a factor used to give the present value of an amount, taking death chance into
+// account. 
+double nEx(char *lt, double i, double charge, double ageX, double ageXn, int corr);
