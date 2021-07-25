1a99b847f426eaee9302e93c8560c392ddfbef11
doctormartin67 <celticfans@hotmail.com>
2021-07-25 14:26:18 +0200

    interpreter tester can print multiple trees
    the interpreter can print files with the [-p] option
    next step will be to be able to free a CaseTree

--- DC/interpreter/interpret.c
+++ DC/interpreter/interpret.c
@@ -5,37 +5,73 @@
 int main(int argc, char *argv[])
 {
     FILE *fp;
-    char s[BUFSIZ];
-    char *t = s;
-    int c, ptree = 0;
+    char **s;
+    long bufsiz;
+    int ptree = 0, treecnt;
     CaseTree *ct;
 
-    if (argc == 1)
+    if (argc < 2)
     {
-	while ((*t = getchar()) != EOF)
-	    t++;
-	*t = '\0';
+	printf("syntax: interpret [-p] file1 [file2] [...]\n");
+	exit(0);
     }
-    else 
+
+    if (argv[1][0] == '-') /* print tree option */
     {
-	if (argv[1][0] == '-' && argv[1][1] == 'p') /* print tree option */
+	if (argc < 3)
 	{
-	    fp = fopen(argv[2], "r");
-	    ptree = 1;
+	    printf("syntax: interpret [-p] file1 [file2] [...]\n");
+	    exit(0);
 	}
-	else
-	    fp = fopen(argv[1], "r");
 
-	while ((c = fgetc(fp)) != EOF)
-	    *t++ = (char )c;
-	*t = '\0';
+	if (strlen(argv[1]) < 2)
+	{
+	    printf("syntax: interpret [-p] file1 [file2] [...]\n");
+	    exit(1);
+	}
+
+	switch(argv[1][1])
+	{
+	    case 'p': ptree = 1;
+		      break;
+
+	    default: 
+		      printf("Unknown option [-%d]\n", argv[1][1]);
+		      exit(1);
+	}
     }
 
-    t = strclean(s);
-    ct = buildTree(t);
+    treecnt = argc - ptree - 1;
+    s = calloc(treecnt, sizeof(char *));
 
-    if (ptree)
-	printTree(ct);
-    
+    for (int i = 0; i < treecnt; i++)
+    {
+	fp = fopen(argv[i + 1 + ptree], "r");
+	if (fp == NULL) errExit("No such file\n");
+
+	if (fseek(fp, 0L, SEEK_END) != 0)
+	    errExit("Unable to move to end of file\n");
+
+	if ((bufsiz = ftell(fp)) == -1)
+	    errExit("Unable to obtain file position\n");
+
+	if ((s[i] = calloc(bufsiz + 1, sizeof(char))) == NULL)
+	    errExit("calloc returned NULL\n");
+
+	if (fseek(fp, 0L, SEEK_SET) != 0)
+	    errExit("Unable to move to beginning of file\n");
+
+	fread(s[i], sizeof(char), bufsiz, fp);
+	/* error handling still to do here */
+
+	s[i][bufsiz] = '\0';
+
+	s[i] = strclean(s[i]);
+	ct = buildTree(s[i]);
+
+	if (ptree)
+	    printTree(ct);
+    }
+
     return 0;
 }
