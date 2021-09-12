d6aa739baa1533881bab09cb81d42e25a5ec7a74
doctormartin67 <celticfans@hotmail.com>
2021-09-12 13:58:53 +0200

    Implemented the function MIN and MAX

--- DC/calculator/calculator.c
+++ DC/calculator/calculator.c
@@ -9,24 +9,42 @@
  * produce the result.
  */
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <math.h>
 #include "errorexit.h"
 #include "libraryheader.h"
 
 enum {STACK_SIZE = 128};
+enum {MAX, MIN, FUNC_AMOUNT};
+
+struct calculator_func {
+	char *name;
+	double (*func)(const char s[static 1]);
+};
 
 static unsigned top = 0;
 static double stack[STACK_SIZE];
 
+unsigned valid_brackets(const char s[static 1]);
+unsigned valid_separators(const char s[static 1]);
 double eval_expr(const char s[static 1]);
 static double addop(const char *s[static 1]);
 static double multop(const char *s[static 1]);
-static unsigned isop(int c);
 static void nextmultop(const char *s[static 1]);
 static void push(double a);
 static double pop(void);
-unsigned valid_brackets(const char s[static 1]);
+static unsigned isfunc(const char s[static 1], unsigned *findex);
+static unsigned infunc(const char s[static 1], const char sep[static 1]);
+static double max(const char s[static 1]);
+static double min(const char s[static 1]);
+
+static struct calculator_func func[FUNC_AMOUNT] = {
+	[MAX] = {"MAX", max},
+	[MIN] = {"MIN", min}
+};
+
 
 int main(void)
 {
@@ -40,14 +58,14 @@ int main(void)
 	}
 
 	if (!valid_brackets(buf)) die("Incorrect brackets");
+	if (!valid_separators(buf)) die("',' outside function");
 
 	printf("%f\n", eval_expr(buf));
 	return 0;
 }
 
 /*
- * evaluates the expression given by s. Assumes the expression does not start
- * with white space
+ * evaluates the expression given by s.
  * The function pushes each addition operator to the stack.
  * This function has indirect recursion because it can be recalled in multop
  * when there are brackets to evaluate the brackets first.
@@ -61,20 +79,18 @@ double eval_expr(const char s[static 1])
 		switch (*s) {
 			case '+' :
 				s++;
-				while (isgarbage(*s)) s++;    
-				if (isop(*s)) die("two ops %c%c", '+', *s);
 				push(addop(&s));
 				pushed++;
 				break;
 			case '-' : 
 				s++;
-				while (isgarbage(*s)) s++;    
-				if (isop(*s)) die("two ops %c%c", '-', *s);
 				push(-addop(&s));
 				pushed++;
 				break;
+			case ',' :
+				if (0 == pushed)
+					die("no expression between ','");
 			case ')' :
-				printf("pushed = %u\n", pushed);
 			case '\0':
 				goto evaluation;
 				break;
@@ -108,6 +124,7 @@ static double addop(const char *s[static 1])
 		switch (**s) {
 			case '+' :
 			case '-' : 
+			case ',' :
 			case ')' :
 			case '\0':
 				return m;
@@ -138,13 +155,15 @@ static double addop(const char *s[static 1])
  * and the pointer is moved to the next point just after the corresponding
  * closed bracket
  * if the operator is followed by a caret '^', it is taken to the power of the
- * next multiplication operator. This means that '^' will be from right to
- * left, f.e. 2^(1+2)^2 = 2^((1+2)^2) and not (2^(1+2))^2
+ * next multiplication operator using recursion.
+ * This means that '^' will be from right to left, 
+ * f.e. 2^(1+2)^2 = 2^((1+2)^2) and not (2^(1+2))^2
  */
 static double multop(const char *s[static 1])
 {
-	const char *t = 0;
+	unsigned findex = 0;
 	int sign = 1;
+	const char *t = 0;
 	double op = 0.0;
 
 	while (isgarbage(**s)) (*s)++;    
@@ -159,40 +178,43 @@ static double multop(const char *s[static 1])
 
 	if ('(' == **s) {
 		(*s)++;
-		while (isgarbage(**s)) (*s)++;
-
 		t = *s;
-
 		nextmultop(s);
 
 		op = sign * eval_expr(t);			
-	} else if (isdigit(**s)) {
-		while (isdigit(**s)) (*s)++;
 
+	} else if (isdigit(**s)) {
+
+		while (isdigit(**s)) (*s)++;
 		if ('.' == **s) {
 			(*s)++;
 			if (!isdigit(**s)) die("Invalid operator");
 			while (isdigit(**s)) (*s)++;
 		}
-
 		while (isgarbage(**s)) (*s)++;
 
 		op = atof(t);
+
+	} else if (isfunc(*s, &findex)) {
+		*s += strlen(func[findex].name);
+		while (isgarbage(**s)) (*s)++;
+		if ('(' != **s) die("Expected '(' after function name");
+		(*s)++;
+		t = *s;
+		nextmultop(s);
+
+		op = sign * func[findex].func(t);
+
 	} else {
 		die("Invalid operator");
 	}
 
 	if ('^' == **s) {
 		(*s)++;
-		op = pow(op, multop(s)); /* CHECK FOR VALID OPERATORS??!! */
+		op = pow(op, multop(s));
 	}
 
 	return op;
-}
-
-static unsigned isop(int c)
-{
-	return '+' == c || '-' == c || '*' == c || '/' == c;
 }
 
 /*
@@ -233,7 +255,6 @@ static void push(double a)
 	} else {
 		stack[top++] = a;
 	}
-	printf("%f pushed\n", a);
 }
 
 static double pop(void)
@@ -241,14 +262,14 @@ static double pop(void)
 	if (0 == top) {
 		die("stack underflow");
 	} else {
-		printf("%f popped\n", stack[top - 1]);
 		return stack[--top];
 	}
 	return 0.0;
 }
 
 /*
- * before the expression is evaulated, the brackets must be checked */
+ * before the expression is evaulated, the brackets must be checked
+ */
 unsigned valid_brackets(const char s[static 1])
 {
 	unsigned br = 0;
@@ -269,4 +290,126 @@ unsigned valid_brackets(const char s[static 1])
 	else validity = 0;
 
 	return validity;
+}
+
+/*
+ * before the expression is evaluated, the ',' must be checked
+ */
+unsigned valid_separators(const char s[static 1])
+{
+	for (const char *sep = strchr(s, ','); sep; sep = strchr(sep, ',')) {
+		if (!infunc(s, sep)) return 0;
+		sep++;
+	}
+
+	return 1;
+}
+
+/*
+ * checks whether the given separator lies within one of the functions in func
+ */
+static unsigned infunc(const char s[static 1], const char sep[static 1])
+{
+	unsigned left = 0, right = 0;
+	int brackets = 0;
+	const char *t = sep;
+
+	while (t > s) {
+		t--;
+		switch (*t) {
+			case '(':
+				brackets++;
+				break;
+			case ')':
+				brackets--;
+				break;
+			default : break;
+		}
+
+		if (1 == brackets) {
+			if (t-- == s) return 0;
+			while (t > s && isgarbage(*t)) t--;
+			while (t > s && isalnum(*t)) t--;
+			if (!isalnum(*t)) t++;
+
+			if (isfunc(t, 0)) left = 1;
+			else return 0;
+
+			break;
+		}
+	}
+	if (!left) return 0;
+
+	t = sep;
+	brackets = 0;
+
+	while ('\0' != *t) {
+		t++;
+		switch (*t) {
+			case '(':
+				brackets++;
+				break;
+			case ')':
+				brackets--;
+				break;
+			default : break;
+		}
+
+		if (-1 == brackets) {
+			right = 1;
+			break;
+		}
+	}
+
+	if (!right) return 0;
+	else return 1;
+}
+
+/*
+ * returns 1 if the string starts with one of the functions defined in func,
+ * 0 otherwise. It will also set *findex to the index of the function given in
+ * func if findex is not NULL
+ */
+static unsigned isfunc(const char s[static 1], unsigned *findex)
+{
+	for (unsigned i = 0; i < FUNC_AMOUNT; i++) {
+		if (0 == strncmp(s, func[i].name, strlen(func[i].name))) {
+			if (findex) *findex = i;
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+static double max(const char s[static 1])
+{
+	double curr = 0;
+	double m = eval_expr(s);
+
+	for (const char *next = strchr(s, ','); next;
+			next = strchr(next, ',')) {
+		next++;
+		if (')' == *next) die("',' directly followed by ')'");
+		curr = eval_expr(next);
+		m = MAX2(m, curr);
+	}
+
+	return m;
+}
+
+static double min(const char s[static 1])
+{
+	double curr = 0;
+	double m = eval_expr(s);
+
+	for (const char *next = strchr(s, ','); next;
+			next = strchr(next, ',')) {
+		next++;
+		if (')' == *next) die("',' directly followed by ')'");
+		curr = eval_expr(next);
+		m = MIN2(m, curr);
+	}
+
+	return m;
 }
